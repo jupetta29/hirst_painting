e1f1dd4c56d693b0466293f4a62ce623aedb76f5
jupetta29 <121144716+jupetta29@users.noreply.github.com>
2023-01-05 04:36:45 -0600

    Add main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+# import colorgram
+#
+# rgb_colors = []
+#
+# colors = colorgram.extract('image.jpg', 30)
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+
+import turtle
+import random
+
+color_list = [(199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 224, 227), (186, 158, 53), (6, 57, 83), (109, 67, 85), (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47), (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186), (172, 153, 159), (212, 183, 177), (176, 198, 203), (150, 115, 120), (202, 185, 190), (40, 72, 82), (46, 73, 62), (47, 66, 82)]
+
+turtle.colormode(255)
+
+screen = turtle.Screen()
+
+tim = turtle.Turtle()
+tim.speed(0)
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    color = random.choice(color_list)
+    tim.dot(20, color)
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen.exitonclick()
